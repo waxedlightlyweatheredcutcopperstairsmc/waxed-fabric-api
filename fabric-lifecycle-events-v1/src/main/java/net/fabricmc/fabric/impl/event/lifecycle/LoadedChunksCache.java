54365983d26cf5dfabeab0a3f1ed2f9927f02bdd
Alice Isabel <roridev@protonmail.com>
2024-01-07 23:14:47 -0300

    Fix LoadedChunksCache

--- fabric-lifecycle-events-v1/src/main/java/net/fabricmc/fabric/impl/event/lifecycle/LoadedChunksCache.java
+++ fabric-lifecycle-events-v1/src/main/java/net/fabricmc/fabric/impl/event/lifecycle/LoadedChunksCache.java
@@ -18,21 +18,21 @@ package net.fabricmc.fabric.impl.event.lifecycle;
 
 import java.util.Set;
 
-import net.minecraft.world.chunk.WorldChunk;
+import net.minecraft.core.world.chunk.Chunk;
 
 /**
  * A simple marker interface which holds references to chunks which block entities may be loaded or unloaded from.
  */
 public interface LoadedChunksCache {
-	Set<WorldChunk> fabric_getLoadedChunks();
+	Set<Chunk> fabric_getLoadedChunks();
 
 	/**
 	 * Marks a chunk as loaded in a world.
 	 */
-	void fabric_markLoaded(WorldChunk chunk);
+	void fabric_markLoaded(Chunk chunk);
 
 	/**
 	 * Marks a chunk as unloaded in a world.
 	 */
-	void fabric_markUnloaded(WorldChunk chunk);
+	void fabric_markUnloaded(Chunk chunk);
 }
