20cb00d80e03f405c1de398f809aa1e63001d223
Alice Isabel <roridev@protonmail.com>
2024-01-07 23:13:50 -0300

    Neuter CommonLifecycleEvents

--- fabric-lifecycle-events-v1/src/main/java/net/fabricmc/fabric/api/event/lifecycle/v1/CommonLifecycleEvents.java
+++ fabric-lifecycle-events-v1/src/main/java/net/fabricmc/fabric/api/event/lifecycle/v1/CommonLifecycleEvents.java
@@ -16,29 +16,28 @@
 
 package net.fabricmc.fabric.api.event.lifecycle.v1;
 
-import net.minecraft.registry.DynamicRegistryManager;
-
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 
+@Deprecated(forRemoval = true)
 public class CommonLifecycleEvents {
 	private CommonLifecycleEvents() {
 	}
 
-	/**
-	 * Called when tags are loaded or updated.
-	 */
-	public static final Event<TagsLoaded> TAGS_LOADED = EventFactory.createArrayBacked(TagsLoaded.class, callbacks -> (registries, client) -> {
-		for (TagsLoaded callback : callbacks) {
-			callback.onTagsLoaded(registries, client);
-		}
-	});
-
-	public interface TagsLoaded {
-		/**
-		 * @param registries Up-to-date registries from which the tags can be retrieved.
-		 * @param client True if the client just received a sync packet, false if the server just (re)loaded the tags.
-		 */
-		void onTagsLoaded(DynamicRegistryManager registries, boolean client);
-	}
+//	/**
+//	 * Called when tags are loaded or updated.
+//	 */
+//	public static final Event<TagsLoaded> TAGS_LOADED = EventFactory.createArrayBacked(TagsLoaded.class, callbacks -> (registries, client) -> {
+//		for (TagsLoaded callback : callbacks) {
+//			callback.onTagsLoaded(registries, client);
+//		}
+//	});
+//
+//	public interface TagsLoaded {
+//		/**
+//		 * @param registries Up-to-date registries from which the tags can be retrieved.
+//		 * @param client True if the client just received a sync packet, false if the server just (re)loaded the tags.
+//		 */
+//		void onTagsLoaded(DynamicRegistryManager registries, boolean client);
+//	}
 }
