78e95d00bc7f851fcea41f752a53f5601b91aba2
Alice Isabel <roridev@protonmail.com>
2024-01-07 23:12:17 -0300

    Fix ServerWorldEvents

--- fabric-lifecycle-events-v1/src/main/java/net/fabricmc/fabric/api/event/lifecycle/v1/ServerWorldEvents.java
+++ fabric-lifecycle-events-v1/src/main/java/net/fabricmc/fabric/api/event/lifecycle/v1/ServerWorldEvents.java
@@ -17,8 +17,7 @@
 package net.fabricmc.fabric.api.event.lifecycle.v1;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.PersistentState;
+import net.minecraft.server.world.WorldServer;
 
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
@@ -27,7 +26,6 @@ public final class ServerWorldEvents {
 	/**
 	 * Called just after a world is loaded by a Minecraft server.
 	 *
-	 * <p>This can be used to load world specific metadata or initialize a {@link PersistentState} on a server world.
 	 */
 	public static final Event<Load> LOAD = EventFactory.createArrayBacked(Load.class, callbacks -> (server, world) -> {
 		for (Load callback : callbacks) {
@@ -49,12 +47,12 @@ public final class ServerWorldEvents {
 
 	@FunctionalInterface
 	public interface Load {
-		void onWorldLoad(MinecraftServer server, ServerWorld world);
+		void onWorldLoad(MinecraftServer server, WorldServer world);
 	}
 
 	@FunctionalInterface
 	public interface Unload {
-		void onWorldUnload(MinecraftServer server, ServerWorld world);
+		void onWorldUnload(MinecraftServer server, WorldServer world);
 	}
 
 	private ServerWorldEvents() {
